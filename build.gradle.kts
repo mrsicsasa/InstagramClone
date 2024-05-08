31fa2ce95d84f18b7578398b59f7de1a87e1add4
mrsicsasa <sasamrsic20@gmail.com>
2024-05-08 15:58:24 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    dependencies {
+        classpath(libs.google.services)
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+    repositories{
+        mavenCentral()
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+
+}
