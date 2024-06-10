a27d0d9c932ac91e34ce0830707a9f8e14bbfe96
mrsicsasa <sasamrsic20@gmail.com>
2024-06-10 18:07:20 +0200

    MyPosts Screen

--- app/src/main/java/com/example/instragramclone/main/MyPostsScreen.kt
+++ app/src/main/java/com/example/instragramclone/main/MyPostsScreen.kt
@@ -1,22 +1,89 @@
 package com.example.instragramclone.main
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.instragramclone.IgViewModel
 
 @Composable
-fun MyPostsScreen(navController: NavController, vm:IgViewModel){
+fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
+    val userData = vm.userData.value
+    val isLoading = vm.inProgress.value
     Column(modifier = Modifier.fillMaxSize()) {
         Column(Modifier.weight(1f)) {
-            Text(text = "MyPosts")
+            Row {
+                ProfileImage(userData?.imageUrl) {
+                }
+                Text(
+                    text = "15\nposts",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "54\nfollowers",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "93\nfollowing",
+                    modifier = Modifier
+                        .weight(1f)
+                        .align(Alignment.CenterVertically),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(modifier = Modifier.padding(8.dp)) {
+                val usernameDisplay =
+                    if (userData?.username == null) "" else "@${userData?.username}"
+                Text(text = userData?.name ?: "", fontWeight = FontWeight.Bold)
+                Text(text = usernameDisplay)
+                Text(text = userData?.bio ?: "")
+            }
+            OutlinedButton(
+                onClick = { },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                elevation = ButtonDefaults.elevatedButtonElevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 0.dp,
+                    disabledElevation = 0.dp
+                ),
+                shape = RoundedCornerShape(10)
+            ) {
+                Text(text = "Edit Profile",)
+            }
+            Column(modifier = Modifier.weight(1f)) {
+                Text("Posts list")
+            }
         }
         BottomNavigationMenu(
             selectedItem = BottomNavigationItem.POSTS,
             navController = navController
         )
     }
+}
+
+@Composable
+fun ProfileImage(imageurl: String?, onClick: () -> Unit) {
+
 }
